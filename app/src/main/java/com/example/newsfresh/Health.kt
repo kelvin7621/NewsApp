96c4ee2fdc76af30b07bed8b0ea533c841c5994f
kelvin7621 <kelvinvaddoriya.kv@gmail.com>
2021-03-20 13:33:01 +0530

    Added new views

---
+++ app/src/main/java/com/example/newsfresh/Health.kt
@@ -0,0 +1,46 @@
+package com.example.newsfresh
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class Health : AppCompatActivity() {
+    private lateinit var toggle: ActionBarDrawerToggle
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_health)
+
+        //Action Bar
+        val intentAboutUs = Intent(this, AboutUS::class.java)
+        val intentBreakingNews = Intent(this, Health::class.java)
+        val intentBusinessNews = Intent(this, BusinessNews::class.java)
+        val intentIndian = Intent(this, MainActivity::class.java)
+
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+            when (it.itemId){
+                R.id.business -> startActivity(intentBusinessNews)
+                R.id.breaking -> startActivity(intentBreakingNews)
+                R.id.indian -> startActivity(intentIndian)
+                R.id.about -> startActivity(intentAboutUs)
+            }
+            true
+        }
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
--- app/src/main/java/com/example/newsfresh/MainActivity.kt
+++ app/src/main/java/com/example/newsfresh/MainActivity.kt
@@ -1,24 +1,47 @@
 package com.example.newsfresh
 
+import android.content.Intent
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.browser.customtabs.CustomTabsIntent
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.android.volley.Request
-import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import kotlinx.android.synthetic.main.activity_main.*
-import org.json.JSONObject
 
 class MainActivity : AppCompatActivity(), NewsItemClicked {
 
     private lateinit var mAdapter: NewsListAdapter
+    private lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Action Bar
+        val intentAboutUs = Intent(this, AboutUS::class.java)
+        val intentBreakingNews = Intent(this, Health::class.java)
+        val intentBusinessNews = Intent(this, BusinessNews::class.java)
+        val intentIndian = Intent(this, MainActivity::class.java)
+
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+            when (it.itemId){
+                R.id.business -> startActivity(intentBusinessNews)
+                R.id.breaking -> startActivity(intentBreakingNews)
+                R.id.indian -> startActivity(intentIndian)
+                R.id.about -> startActivity(intentAboutUs)
+            }
+            true
+        }
 
         recyclerView.layoutManager = LinearLayoutManager(this)
         fetchData()
@@ -27,12 +50,12 @@ class MainActivity : AppCompatActivity(), NewsItemClicked {
     }
 
     private fun fetchData() {
-        val url = "https://newsapi.org/v2/top-headlines?country=in&category=science&piKey=1f4a12d2698e432ea9cf18126dcc7acd"
+        val url = "https://saurav.tech/NewsAPI/top-headlines/category/general/in.json"
         val jsonObjectRequest = JsonObjectRequest(
             Request.Method.GET,
             url,
             null,
-            Response.Listener {
+            {
                 val newsJsonArray = it.getJSONArray("articles")
                 val newsArray = ArrayList<News>()
                 for(i in 0 until newsJsonArray.length()) {
@@ -48,7 +71,7 @@ class MainActivity : AppCompatActivity(), NewsItemClicked {
 
                 mAdapter.updateNews(newsArray)
             },
-            Response.ErrorListener {
+            {
 
             }
         )
@@ -59,5 +82,12 @@ class MainActivity : AppCompatActivity(), NewsItemClicked {
         val builder =  CustomTabsIntent.Builder()
         val customTabsIntent = builder.build()
         customTabsIntent.launchUrl(this, Uri.parse(item.url))
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
